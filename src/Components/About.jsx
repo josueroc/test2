0e722fa682043c2caa164294bcfbd92c835cd4f8
Josué <112716052+josueroc@users.noreply.github.com>
2024-12-21 00:25:09 +0000

    cambio de imagen

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/cherries.jpeg";
+import image from "../images/black lace.jpeg";
 
-const imageAltText = "cherries background";
+const imageAltText = "black lace background";
 
 /**
  * Sort description that expands on your title on the Home component.
